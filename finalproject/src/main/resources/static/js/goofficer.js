c0f23e5eab3a9bb67dbc972e765f35b498e9af5e
lee1066515 <lee1066515@naver.com>
2024-02-03 16:58:34 +0900

    2024-02-03(마커 크기 45로 변동)

--- finalproject/src/main/resources/static/js/goofficer.js
+++ finalproject/src/main/resources/static/js/goofficer.js
@@ -171,7 +171,7 @@ function clearMarkers() {
    // 내 위치 마커 이미지 정의
    var myLocationImage = new kakao.maps.MarkerImage(
     'https://playdataroads.s3.ap-northeast-2.amazonaws.com/iconimage/free-icon-gps-navigation-5142952.png',
-    new kakao.maps.Size(25, 25), // 마커 이미지 크기
+    new kakao.maps.Size(45, 45), // 마커 이미지 크기
     { offset: new kakao.maps.Point(15, 15) } // 마커 이미지 좌표 설정 (가운데 정렬을 위해)
   );
   //console.log("결과2:",myLocationImage);
